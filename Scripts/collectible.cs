680a16ddb702fd39943b3368ad0c45e18fffbcfb
ZorbeyOnurAk <weisshagane@gmail.com>
2024-05-11 12:16:35 +0300

    singal

--- Scripts/collectible.cs
+++ Scripts/collectible.cs
@@ -3,13 +3,17 @@ using System;
 
 public partial class collectible : Area2D
 {
-	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 	}
+}
+
+
+private void _on_body_entered(Node2D body)
+{
+	// Replace with function body.
 }
